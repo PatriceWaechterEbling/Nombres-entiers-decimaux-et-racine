85a157bb2f91dfdc79e4b28238ee68037b5d50fe
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2023-04-08 23:13:50 -0400

    Add files via upload

---
+++ Nombres entiers et decimaux_racine enieme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace RacineEnieme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Bienvenue au cours d AEC\nRacine Carrées";
+            Console.ForegroundColor = ConsoleColor.Cyan | ConsoleColor.DarkGray;
+            Console.WriteLine("\n\nEntrez le radical ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entrez le facteur N ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double b = System.Math.Pow(x, 1 / y);
+            Console.WriteLine("La racine enieme " + y +" de " + x + " est: " + b);
+            Console.ReadLine();
+        }
+    }
+}
